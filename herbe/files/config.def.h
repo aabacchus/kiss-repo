df319c47a08fd6c0981241384efc3ff0b091c994
aabacchus <ben@bvnf.space>
2022-07-18 19:18:57 +0100

    herbe: update config

--- herbe/files/config.def.h
+++ herbe/files/config.def.h
@@ -1,19 +1,19 @@
-static const char *background_color = "#3e3e3e";
+static const char *background_color = "#000000";
 static const char *border_color = "#99991c";
-static const char *font_color = "#ececec";
-static const char *font_pattern = "terminus:size=16";
-static const unsigned line_spacing = 5;
-static const unsigned int padding = 15;
+static const char *font_color = "#ffffff";
+static const char *font_pattern = "monospace:size=14";
+static const unsigned line_spacing = 9;
+static const unsigned int padding = 10;
 
-static const unsigned int width = 450;
-static const unsigned int border_size = 5;
+static const unsigned int width = 350;
+static const unsigned int border_size = 2;
 static const unsigned int pos_x = 30;
 static const unsigned int pos_y = 60;
 
 enum corners { TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT };
 enum corners corner = TOP_LEFT;
 
-static const unsigned int duration = 5; /* in seconds */
+static const unsigned int duration = 2; /* in seconds */
 
 #define DISMISS_BUTTON Button1
 #define ACTION_BUTTON Button3
