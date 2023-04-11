c21e713e933cb19a62e204e9d524656b82f08a84
aabacchus <ben@bvnf.space>
2023-04-11 23:16:17 +0100

    herbe: 3s timeout

--- herbe/files/config.def.h
+++ herbe/files/config.def.h
@@ -13,7 +13,7 @@ static const unsigned int pos_y = 60;
 enum corners { TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT };
 enum corners corner = TOP_LEFT;
 
-static const unsigned int duration = 2; /* in seconds */
+static const unsigned int duration = 3; /* in seconds */
 
 #define DISMISS_BUTTON Button1
 #define ACTION_BUTTON Button3
